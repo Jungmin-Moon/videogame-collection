4c38eef33eae852c5c883d23c6e04bd887d0966b
Jungmin Moon <jmmoon96@gmail.com>
2024-07-20 01:46:47 -0400

    Test Commit

---
+++ videogame_collection/src/main/java/com/example/videogame_collection/VideogameCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.videogame_collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogameCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogameCollectionApplication.class, args);
+	}
+
+}
---
+++ videogame_collection/src/test/java/com/example/videogame_collection/VideogameCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videogame_collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogameCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
