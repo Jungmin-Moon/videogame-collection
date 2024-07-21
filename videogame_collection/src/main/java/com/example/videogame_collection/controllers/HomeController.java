4a51e0ea253d5186e8454b2ed3a0b245766dbbe4
Jungmin Moon <jmmoon96@gmail.com>
2024-07-21 16:09:45 -0400

    Starting creation of classes and html.

---
+++ videogame_collection/src/main/java/com/example/videogame_collection/controllers/HomeController.java
@@ -0,0 +1,8 @@
+package com.example.videogame_collection.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+	
+}
---
+++ videogame_collection/src/main/java/com/example/videogame_collection/model/Account.java
@@ -0,0 +1,9 @@
+package com.example.videogame_collection.model;
+
+public class Account {
+	
+	private String username;
+	private String password;
+	
+	
+}
