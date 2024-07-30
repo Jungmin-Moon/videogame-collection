9c2259f1593f0d72b0bdcce0cb39b98e5dc006cd
Jungmin Moon <jmmoon96@gmail.com>
2024-07-30 03:19:55 -0400

    boiler plate for register controller

--- videogame_collection/src/main/java/com/example/videogame_collection/controllers/RegisterController.java
+++ videogame_collection/src/main/java/com/example/videogame_collection/controllers/RegisterController.java
@@ -1,7 +1,10 @@
 package com.example.videogame_collection.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RegisterController {
@@ -9,6 +12,17 @@ public class RegisterController {
 	
 	@GetMapping("/register")
 	public String register() {
+		return "register.html";
+	}
+	
+	
+	
+	@PostMapping("/register")
+	public String registerPost(@RequestParam String username, @RequestParam String password, @RequestParam String passwordAgain, 
+								Model model) {
+		
+		
+		
 		
 		
 		return "register.html";
