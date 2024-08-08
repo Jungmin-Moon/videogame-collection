873c09d220df2c97e6d9164ad16e4b3130a1f727
Jungmin Moon <jmmoon96@gmail.com>
2024-08-07 22:20:36 -0400

    FInished login and will need to test to make sure it all works out

--- videogame_collection/src/main/java/com/example/videogame_collection/controllers/LoginController.java
+++ videogame_collection/src/main/java/com/example/videogame_collection/controllers/LoginController.java
@@ -1,15 +1,43 @@
 package com.example.videogame_collection.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.videogame_collection.services.LoginService;
 
 @Controller
 public class LoginController {
 
+	private final LoginService loginService;
+	
+	LoginController(LoginService loginService) {
+		this.loginService = loginService;
+	}
 	
 	@GetMapping("/login")
 	public String login() {
+		return "login.html";
+	}
+	
+	
+	
+	@PostMapping("/login")
+	public String loginPost(@RequestParam String username, @RequestParam String password, Model model) {
 		
+		
+		loginService.setUsername(username);
+		loginService.setPassword(password);
+		
+		boolean logged = loginService.login(username, password);
+		
+		if (logged) {
+			return "redirect:/profile";
+		}
+		
+		model.addAttribute("message", "Incorrect credentials");
 		
 		return "login.html";
 	}
