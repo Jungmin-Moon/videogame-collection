00a4fea94ce042cc64953d43394866fe5a09bad0
Jungmin Moon <jmmoon96@gmail.com>
2024-08-24 22:45:44 -0400

    getmapping for the page is the same as profile and thus just needed to
    be copied over.

--- videogame_collection/src/main/java/com/example/videogame_collection/controllers/ModifyController.java
+++ videogame_collection/src/main/java/com/example/videogame_collection/controllers/ModifyController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.videogame_collection.services.GameService;
@@ -22,8 +23,29 @@ public class ModifyController {
 	@GetMapping("/modify")
 	public String modifyGet(@RequestParam(required = false) String logout, @RequestParam(required = false) String back, Model model) {
 		
+		if (logout != null) {
+			loginManager.setUsername(null);
+		}
 		
+		String username = loginManager.getUserName();
+		
+		if (username == null) {
+			return "redirect:/home";
+		}
+		
+		var games = gameService.getGames(username);
+		
+		model.addAttribute("username", username);
+		model.addAttribute("games", games); 
 		
 		return "modify.html";
+	}
+	
+	
+	@PostMapping("/modify") 
+	public String modifyPost() {
+		
+		
+		return "redirect:/profile";
 	}
 }
