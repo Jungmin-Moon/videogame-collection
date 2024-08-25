0f25099e78a7b60ca2560dc941f9bde87d8f48e1
Jungmin Moon <jmmoon96@gmail.com>
2024-08-24 22:31:25 -0400

    Starting controller for modify page

--- videogame_collection/src/main/java/com/example/videogame_collection/controllers/ModifyController.java
+++ videogame_collection/src/main/java/com/example/videogame_collection/controllers/ModifyController.java
@@ -1,5 +1,29 @@
 package com.example.videogame_collection.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.videogame_collection.services.GameService;
+import com.example.videogame_collection.services.LoginManager;
+
+@Controller
 public class ModifyController {
 
+	@Autowired
+	private LoginManager loginManager;
+	
+	@Autowired
+	private GameService gameService;
+	
+	
+	@GetMapping("/modify")
+	public String modifyGet(@RequestParam(required = false) String logout, @RequestParam(required = false) String back, Model model) {
+		
+		
+		
+		return "modify.html";
+	}
 }
